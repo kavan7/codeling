17b9490f6b991a38e7abbfac887408ad10d8ce06
kavan7 <abeyratnekavan@gmail.com>
2024-01-27 12:15:22 -0500

    initial

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,12 @@
+import { pages } from "next/dist/build/templates/app-page"
+
+async function Page() {
+    return(
+        <main>
+            <h1 className="head-text">Onboarding</h1>
+        </main>
+
+    )
+}
+
+export default Page;
