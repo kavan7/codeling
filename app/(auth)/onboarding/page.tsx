a1a024da951dbc971fa1641aae8061862037580b
kavan7 <abeyratnekavan@gmail.com>
2024-01-30 13:59:28 -0500

    account profile

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -5,7 +5,17 @@ import { currentUser } from "@clerk/nextjs";
 async function Page() {
     const user = await currentUser();
 
-    const userData = {}
+    const userInfo = {};
+    const userData = {
+        id: user?.id,
+        objectId: userInfo?._id,
+        username: userInfo?.username || user?.username,
+        name : userInfo?.name || user?.firstName || "",
+        bio: userInfo?.bio || "",
+        image: userInfo?.image : user?.imageUrl,
+
+
+    }
     return(
         <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
             <h1 className="head-text">Onboarding</h1>
@@ -15,7 +25,8 @@ async function Page() {
             </p>
 
             <section className="mt-9 bg-dark-2 p-10">
-                <AccountProfile />
+                <AccountProfile  user={userData}
+                btnTitle="Continue"/>
             </section>
         </main>
 
