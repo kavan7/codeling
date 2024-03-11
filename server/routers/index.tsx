5d2da7799d17d530cd8b12de2f31e3395545d5e8
kavan7 <abeyratnekavan@gmail.com>
2024-03-10 23:15:20 -0400

    chat

--- server/routers/index.tsx
+++
@@ -1,9 +0,0 @@
-import { router, protectedProcedure } from '../trpc'
- 
-export const protectedRouter = router({
-  hello: protectedProcedure.query(({ ctx }) => {
-    return {
-      secret: `${ctx.auth?.userId} is using a protected procedure`
-    }
-  })
-})
