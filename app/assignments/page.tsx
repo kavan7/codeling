d206dce904d30656dfc59e2fcfdd884e0d859c46
kavan7 <abeyratnekavan@gmail.com>
2024-03-10 23:17:38 -0400

    closing tag

--- app/assignments/page.tsx
+++ app/assignments/page.tsx
@@ -142,7 +142,7 @@ export default function Home(){
 
     <Accordion type="multiple"  className="w-full">
   <AccordionItem value="item-1"> {/*Day One*/}
-    <AccordionTrigger>Day 1 
+    <AccordionTrigger>Day 1 </AccordionTrigger>
         <AccordionContent>
         <AccordionItem value="item-6" >  <AccordionTrigger>{`    > `}Using Replit</AccordionTrigger><AccordionContent className="text-slate-300">In this course, we will be using an online python compiler called Repl.it. <br/>Please visit repl.it and start a new python project.</AccordionContent></AccordionItem>
         </AccordionContent>
